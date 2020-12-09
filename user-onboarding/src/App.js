1e18c2247a00df27d1203da9a12bf17fdaf41faf
Dominick Groven <36062458+DominickGroven@users.noreply.github.com>
2020-12-08 19:45:14 -0800

    Finished

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+import Form from "./Form.js";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Super High TEch FOrm</p>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
